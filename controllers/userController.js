1ea821cdd962297f20a7480daded82b375fbd1f3
OluRemiFour <lekanayoola04@gmail.com>
2026-01-04 14:41:00 +0100

    updates

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,7 @@
 const User = require("../models/User");
 
-// controllers/userController.js
 const getUser = async (req, res) => {
   try {
-    // req.user is already attached by the protect middleware
     if (!req.userId) {
       return res.status(401).json({
         statusCode: "01",
@@ -11,13 +9,19 @@ const getUser = async (req, res) => {
       });
     }
 
-    const user = await User.findById(req.userId);
+    const user = await User.findById(req.userId).select("-password"); // Don't send password
+
+    if (!user) {
+      return res.status(404).json({
+        statusCode: "01",
+        message: "User not found",
+      });
+    }
 
     return res.status(200).json({
       statusCode: "00",
       message: "User retrieved successfully",
-      user: req.userId,
-      data: user,
+      user: user,
     });
   } catch (error) {
     console.error("Get me error:", error);
